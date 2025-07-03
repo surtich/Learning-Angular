8e2c4f3f261fe49bce7ce2095a08b611461dab2b
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 00:23:18 +0200

    ch06 - Usando observables se pueden emitir varios valores. En este caso, no nos interesa el valor emitido sino el paso del tiempo.
    El convenio es que los observables terminen en  `$` como en `title$`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS, appSettings } from './app.settings';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +16,11 @@ export class AppComponent {
   title = 'World';
   settings = inject(APP_SETTINGS);
 
-  private onComplete() {
-    return new Promise<void>((resolve) => {
-      setInterval(() => {
-        resolve();
-      }, 2000);
-    });
-  }
+  title$ = new Observable<void>((observer) => {
+    setInterval(() => {
+      observer.next();
+    }, 2000);
+  });
 
   private setTitle = () => {
     const timestamp = new Date();
@@ -29,6 +28,6 @@ export class AppComponent {
   };
 
   constructor() {
-    this.onComplete().then(this.setTitle);
+    this.title$.subscribe(this.setTitle);
   }
 }
