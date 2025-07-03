7f3df4f45a45d35ca8eaf1607463b751bce1d904
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 21:48:28 +0200

    ch05 - Otra forma de inyectar servicios a nivel de Root, es incluirlo como proveedor en el fichero `app.config.ts`.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ProductsService } from './products';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    ProductsService,
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,9 +1,5 @@
-import { Injectable } from '@angular/core';
 import { Product } from './product';
 
-@Injectable({
-  providedIn: 'root',
-})
 export class ProductsService {
   constructor() {}
 
