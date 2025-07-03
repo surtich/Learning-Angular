47c3a0f8e906257ef05325756c564f1dec347d98
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 21:10:08 +0200

    ch04 - Se explica el concepto de directivas y se crea una directiva personalizada para mostrar un mensaje de copyright.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductList } from './product-list/product-list';
+import { CopyrightDirective } from './copyright';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductList],
+  imports: [CopyrightDirective, RouterOutlet, ProductList],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'World';
---
+++ src/app/copyright.spec.ts
@@ -0,0 +1,8 @@
+import { Copyright } from './copyright';
+
+describe('Copyright', () => {
+  it('should create an instance', () => {
+    const directive = new Copyright();
+    expect(directive).toBeTruthy();
+  });
+});
