6ac15e0353bcb4d434cf6b927800913faa850476
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 00:43:25 +0200

    ch06 - Se añade un componente key-logger para capturar eventos de teclado
    Este cambio ilustra cómo utilizar el operador fromEvent de RxJS para manejar eventos del DOM. Se crea un observable que registra las teclas pulsadas en un campo de entrada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS, appSettings } from './app.settings';
 import { timer } from 'rxjs';
+import { KeyLoggerComponent } from './key-logger/key-logger.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
+  imports: [KeyLoggerComponent, RouterOutlet, ProductListComponent, CopyrightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
---
+++ src/app/key-logger/key-logger.component.ts
@@ -0,0 +1,22 @@
+import { Component, ElementRef, OnInit, viewChild, input } from '@angular/core';
+import { fromEvent, tap, map, filter } from 'rxjs';
+
+@Component({
+  selector: 'app-key-logger',
+  imports: [],
+  templateUrl: './key-logger.component.html',
+  styleUrl: './key-logger.component.css',
+})
+export class KeyLoggerComponent implements OnInit {
+  input = viewChild<ElementRef>('keyContainer'); // viewChild a la template reference variable #keyContainer
+  numeric = input(false);
+  keys = '';
+
+  ngOnInit(): void {
+    const logger$ = fromEvent<KeyboardEvent>(
+      this.input()!.nativeElement, // Acceso al elemento nativo del DOM. viewChild devuelve un wrapper
+      'keyup',
+    );
+    logger$.subscribe((evt) => (this.keys += evt.key));
+  }
+}
