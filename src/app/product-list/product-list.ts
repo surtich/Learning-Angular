cd28d5e34875a9209074d4d6e5d63a6e40dbf549
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 17:18:39 +0200

    ch03 - Se explica el uso de @if
    Se actualiza Prettier ya que con la versión instalada no formatea bien los fichero .html con bloques @if.

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
   imports: [],
   templateUrl: './product-list.html',
-  styleUrl: './product-list.css'
+  styleUrl: './product-list.css',
 })
 export class ProductList {
-
+  products: Product[] = [];
 }
