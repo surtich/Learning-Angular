88037c86489c7a114d71a61ac080a47d59eacfb7
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 17:51:59 +0200

    ch03 - Se explica el uso del bloque @for para recorrer colecciones.
    Se actualiza el componente de lista de productos con datos de ejemplo.

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -8,5 +8,10 @@ import { Product } from '../product';
   styleUrl: './product-list.css',
 })
 export class ProductList {
-  products: Product[] = [];
+  products: Product[] = [
+    { id: 1, title: 'Keyboard' },
+    { id: 2, title: 'Microphone' },
+    { id: 3, title: 'Web camera' },
+    { id: 4, title: 'Tablet' },
+  ];
 }
