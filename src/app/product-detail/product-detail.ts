0f9858afe43fd9db85614154629b99ec0b120f17
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 19:19:56 +0200

    ch03 - Ahora se constata que el hook OnInit contiene el valor del producto inicial. Sin embargo, este evento no se vuelve a disparar cuando se cambia el producto seleccionado.

--- src/app/product-detail/product-detail.ts
+++ src/app/product-detail/product-detail.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, OnInit, output } from '@angular/core';
 import { Product } from '../product';
 
 @Component({
@@ -8,12 +8,16 @@ import { Product } from '../product';
   styleUrl: './product-detail.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit {
   product = input<Product>();
   added = output();
 
   constructor() {
-    console.log('Product:', this.product());
+    console.log('Product Constructor:', this.product());
+  }
+
+  ngOnInit() {
+    console.log('Product onInit:', this.product());
   }
 
   addToCart() {
--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -9,13 +9,13 @@ import { ProductDetail } from '../product-detail/product-detail';
   styleUrl: './product-list.css',
 })
 export class ProductList {
-  selectedProduct: Product | undefined;
   products: Product[] = [
     { id: 1, title: 'Keyboard' },
     { id: 2, title: 'Microphone' },
     { id: 3, title: 'Web camera' },
     { id: 4, title: 'Tablet' },
   ];
+  selectedProduct: Product | undefined = this.products[0];
   onAdded() {
     alert(`${this.selectedProduct?.title} added to the cart!`);
   }
