fa393bd28da50687664edb7833c988998f17e9fa
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 23:37:38 +0200

    ch05 - Se añade explicación sobre `useValue` para servicios
    Se introduce el uso de `InjectionToken` para inyectar configuraciones de aplicación como servicio. Esto permite transformar objetos en servicios en Angular.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductList } from './product-list/product-list';
 import { CopyrightDirective } from './copyright';
-import { NumericDirective } from './numeric';
+import { APP_SETTINGS, appSettings } from './app.settings';
 
 @Component({
   selector: 'app-root',
   imports: [CopyrightDirective, RouterOutlet, ProductList],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class App {
   protected title = 'World';
+  settings = inject(APP_SETTINGS);
 }
