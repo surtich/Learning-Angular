869cdbbae114e369bcb9b26ef4c8929cbf61f121
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 01:58:12 +0200

    Una forma alternativa de hacer la desuscripción es usar `takeUntilDestroyed` de `@angular/core/rxjs-interop`, que se encarga de limpiar automáticamente las suscripciones cuando el componente se destruye.

--- src/app/key-logger/key-logger.component.ts
+++ src/app/key-logger/key-logger.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, OnInit, viewChild, input, OnDestroy } from '@angular/core';
-import { fromEvent, tap, map, filter, Subscription } from 'rxjs';
+import { Component, ElementRef, OnInit, viewChild, input, inject, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { fromEvent, tap, map, filter } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
@@ -7,18 +8,18 @@ import { fromEvent, tap, map, filter, Subscription } from 'rxjs';
   templateUrl: './key-logger.component.html',
   styleUrl: './key-logger.component.css',
 })
-export class KeyLoggerComponent implements OnInit, OnDestroy {
+export class KeyLoggerComponent implements OnInit {
   input = viewChild<ElementRef>('keyContainer');
   numeric = input(false);
   keys = '';
-  private loggerSub: Subscription | undefined;
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const logger$ = fromEvent<KeyboardEvent>(
       this.input()!.nativeElement,
       'keyup',
     );
-    this.loggerSub = logger$
+    logger$
       .pipe(
         map((evt) => evt.key.charCodeAt(0)),
         filter((code) => {
@@ -28,11 +29,8 @@ export class KeyLoggerComponent implements OnInit, OnDestroy {
           return true;
         }),
         tap((digit) => (this.keys += String.fromCharCode(digit))),
+        takeUntilDestroyed(this.destroyRef),
       )
       .subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.loggerSub?.unsubscribe();
   }
 }
