98538b083d650a9db959fe2a0e1d16dd16072e33
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-03 19:15:31 +0200

    ch03 - Se añade explicación sobre el ciclo de vida de componentes
    Se intenta acceder a una propiedad input en el constructor. Esto no es correcto ya que las propiedades todavía no están inicializadas en ese momento. Se puede constatar ya que en la consola se muestra:
    
    Product: undefined

--- src/app/product-detail/product-detail.ts
+++ src/app/product-detail/product-detail.ts
@@ -11,9 +11,15 @@ import { Product } from '../product';
 export class ProductDetail {
   product = input<Product>();
   added = output();
+
+  constructor() {
+    console.log('Product:', this.product());
+  }
+
   addToCart() {
     this.added.emit();
   }
+
   get productTitle() {
     return this.product()!.title;
   }
