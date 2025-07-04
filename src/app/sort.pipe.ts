d93695a06b242696c9b30c688643d194bf899dd4
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 13:29:32 +0200

    ch08 - Una solución es utilizar un `pipe` impuro, aunque esto degrada el rendimiento y, además, no soluciona el problema de `add`.

--- src/app/sort.pipe.ts
+++ src/app/sort.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Product } from './product';
 
 @Pipe({
-  name: 'sort'
+  name: 'sort',
+  pure: false,
 })
 export class SortPipe implements PipeTransform {
   transform(value: Product[], args: keyof Product = 'title'): Product[] {
