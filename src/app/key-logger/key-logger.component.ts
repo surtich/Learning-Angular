e1a3d1bdb1515e8e70f11f76950eaab390691283
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 02:09:24 +0200

    ch06 - Una forma sencilla de suscribirse y desuscribirse es usar la `pipe async` de Angular. La limitación es que solo se puede usar en el `template`.

--- src/app/key-logger/key-logger.component.ts
+++ src/app/key-logger/key-logger.component.ts
@@ -1,36 +1,33 @@
-import { Component, ElementRef, OnInit, viewChild, input, inject, DestroyRef } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { fromEvent, tap, map, filter } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Component, ElementRef, OnInit, viewChild, input } from '@angular/core';
+import { fromEvent, map, filter, Observable, scan } from 'rxjs';
 
 @Component({
   selector: 'app-key-logger',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './key-logger.component.html',
   styleUrl: './key-logger.component.css',
 })
 export class KeyLoggerComponent implements OnInit {
-  input = viewChild<ElementRef>('keyContainer');
+  input = viewChild<ElementRef>('keyContainer'); 
   numeric = input(false);
-  keys = '';
-  private destroyRef = inject(DestroyRef);
+  key$: Observable<string> | undefined;
 
   ngOnInit(): void {
     const logger$ = fromEvent<KeyboardEvent>(
       this.input()!.nativeElement,
       'keyup',
     );
-    logger$
-      .pipe(
-        map((evt) => evt.key.charCodeAt(0)),
-        filter((code) => {
-          if (this.numeric()) {
-            return (code > 31 && (code < 48 || code > 57)) === false;
-          }
-          return true;
-        }),
-        tap((digit) => (this.keys += String.fromCharCode(digit))),
-        takeUntilDestroyed(this.destroyRef),
-      )
-      .subscribe();
+    this.key$ = logger$.pipe(
+      map((evt) => evt.key.charCodeAt(0)),
+      filter((code) => {
+        if (this.numeric()) {
+          return (code > 31 && (code < 48 || code > 57)) === false;
+        }
+        return true;
+      }),
+      map((digit) => String.fromCharCode(digit)),
+      scan((acc, curr) => acc + curr, ''), // <-- Acumula las teclas presionadas
+    );
   }
 }
