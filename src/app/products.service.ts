4dc52f9bdd838d2b788735fcbbce39a7a23f3cff
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 12:10:03 +0200

    ch08 - Se limita el número de productos recuperados a 10 utilizando HttpParams

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -2,17 +2,20 @@ import { inject, Injectable } from '@angular/core';
 import { concatMap, delay, from, Observable, of } from 'rxjs';
 import { Product } from './product';
 import { APP_SETTINGS } from './app.settings';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   private productsUrl = inject(APP_SETTINGS).apiUrl + '/products';
-  
+
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl);
+    const options = new HttpParams().set('limit', 10);
+    return this.http.get<Product[]>(this.productsUrl, {
+      params: options
+    });
   }
 }
