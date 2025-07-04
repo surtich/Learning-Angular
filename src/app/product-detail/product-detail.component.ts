833e511bfa34302ac80245802cbab8d6ecdcaca0
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 23:43:19 +0200

    ch10 - Mejora la experiencia del usuario al mostrar el precio actual mientras se está editando.
    Se modifica el componente de detalle del producto para que el precio se muestre correctamente, utilizando una variable auxiliar en lugar de enlazar directamente al producto. Esto permite que el usuario vea el precio actual en todo momento.

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -16,6 +16,7 @@ import { FormsModule } from '@angular/forms';
 export class ProductDetailComponent implements OnInit {
   id = input<number>();
   product$: Observable<Product> | undefined;
+  price: number | undefined;
 
   constructor(
     private productService: ProductsService,
@@ -36,7 +37,7 @@ export class ProductDetailComponent implements OnInit {
 
   changePrice(product: Product) {
     this.productService
-      .updateProduct(product.id, product.price)
+      .updateProduct(product.id, this.price!)
       .subscribe(() => this.router.navigate(['/products']));
   }
 
