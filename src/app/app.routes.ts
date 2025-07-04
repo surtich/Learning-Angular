e6b6b0cfe9e31108bcb2cdce32a03b15878c3be3
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 18:35:53 +0200

    ch09 - Se añade redirección a la ruta por defecto Se redirige a 'products' al navegar  a '/'.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,9 @@ export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'cart', component: CartComponent },
   { path: 'products/new', component: ProductCreateComponent },
+  // le decimos al router que redirija a la ruta `products` cuando la aplicación navegue a la ruta por defecto.
+  // Una ruta con una cadena de ruta vacía es la ruta por defecto de la aplicación Angular.
+  // La propiedad `pathMatch` le dice al router cómo hacer coincidir la URL con la propiedad de ruta raíz. En este caso, el router redirige a la ruta `products` solo cuando la URL coincide con la ruta raíz, que es la cadena vacía.
+  // Añadimos la ruta por defecto después de todas las demás rutas porque el orden de las rutas es importante. El router selecciona las rutas con una estrategia de "la primera que coincide, gana". Las rutas más específicas deben definirse antes que las menos específicas.
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
 ];
