4f13fd69169f03614ae9cdce09203e48d2896114
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 23:56:01 +0200

    ch10 - Se implementa el uso de formularios reactivos en Angular en la creación de productos
    En este caso, se puede usar el valor del formulario porque el modelo del formulario es idéntico a la interfaz `Product`.
    Si fuera diferente, se podría usar la propiedad `controls` de la clase `FormGroup` para obtener los valores de los controles del formulario individualmente de la siguiente manera:
    
    ```typescript
    createProduct() {
      this.productsService.addProduct(this.productForm.value).
      subscribe(() => {
        this.router.navigate(['/products']);
      });
    }
    ```

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,26 +1,35 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductsService } from '../products.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
 export class ProductCreateComponent {
+  productForm = new FormGroup({
+    title: new FormControl('', { nonNullable: true }),
+    price: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+    }),
+    category: new FormControl('', { nonNullable: true }),
+  });
+
   constructor(
     private productsService: ProductsService,
     private router: Router,
   ) {}
 
-  createProduct(title: string, price: string, category: string) {
-    this.productsService
-      .addProduct({
-        title,
-        price: Number(price),
-        category,
-      })
-      .subscribe(() => this.router.navigate(['/products']));
+  createProduct() {
+    /*
+    La propiedad `this.productForm.value` no incluye valores de campos deshabilitados de un formulario.
+    En su lugar, se puede utilizar el método `getRawValue` para devolver valores de todos los campos.
+    */
+    this.productsService.addProduct(this.productForm.value).subscribe(() => {
+      this.router.navigate(['/products']);
+    });
   }
 }
