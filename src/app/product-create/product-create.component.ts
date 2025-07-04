5cfbee7b6a86eb7ed8d9e9595f6bb5fb647d42fe
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-05 01:15:35 +0200

    ch10 - Se implementa validación global con CSS.

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductsService } from '../products.service';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
@@ -9,46 +9,26 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angul
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
-export class ProductCreateComponent implements OnInit {
-  productForm:
-    | FormGroup<{
-        title: FormControl<string>;
-        price: FormControl<number | undefined>;
-        category: FormControl<string>;
-      }>
-    | undefined;
+export class ProductCreateComponent {
+  productForm = new FormGroup({
+    title: new FormControl('', { nonNullable: true }),
+    price: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+    }),
+    category: new FormControl('', { nonNullable: true }),
+  });
 
   constructor(
     private productsService: ProductsService,
     private router: Router,
-    private builder: FormBuilder,
   ) {}
-
-  private buildForm() {
-    /*
-    Se utiliza la propiedad `nonNullable` de la clase `FormBuilder` para crear un grupo de formulario que no puede ser nulo.
-      El método `group` se utiliza para agrupar controles de formulario.
-      Los controles de formulario `title` y `category` se crean usando una cadena vacía como valor predeterminado.
-      El control de formulario `price` sigue un enfoque diferente al resto porque no se puede asignar un valor predeterminado de `undefined` debido a las limitaciones del lenguaje TypeScript.
-      En este caso, se utiliza el método `control` de la propiedad `nonNullable` para definir el control de formulario.
-    */
-    this.productForm = this.builder.nonNullable.group({
-      title: [''],
-      price: this.builder.nonNullable.control<number | undefined>(undefined),
-      category: [''],
-    });
-  }
-
-  ngOnInit(): void {
-    this.buildForm();
-  }
 
   createProduct() {
     /*
     La propiedad `this.productForm.value` no incluye valores de campos deshabilitados de un formulario.
     En su lugar, se puede utilizar el método `getRawValue` para devolver valores de todos los campos.
     */
-    this.productsService.addProduct(this.productForm!.value).subscribe(() => {
+    this.productsService.addProduct(this.productForm.value).subscribe(() => {
       this.router.navigate(['/products']);
     });
   }
