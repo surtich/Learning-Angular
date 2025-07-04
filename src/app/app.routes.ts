daee1471a6bce9819a2255e444b3c75627f7544a
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-04 18:39:48 +0200

    ch09 - Se añade ruta comodín para manejar errores 404 Se implementa una ruta comodín que redirige a la ruta de productos, asegurando que los usuarios sean guiados correctamente en caso de rutas no coincidentes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,4 +12,6 @@ export const routes: Routes = [
   // La propiedad `pathMatch` le dice al router cómo hacer coincidir la URL con la propiedad de ruta raíz. En este caso, el router redirige a la ruta `products` solo cuando la URL coincide con la ruta raíz, que es la cadena vacía.
   // Añadimos la ruta por defecto después de todas las demás rutas porque el orden de las rutas es importante. El router selecciona las rutas con una estrategia de "la primera que coincide, gana". Las rutas más específicas deben definirse antes que las menos específicas.
   { path: '', redirectTo: 'products', pathMatch: 'full' },
+  // La ruta comodín debe ser la última entrada en la lista de rutas porque la aplicación solo debe alcanzarla si no hay rutas coincidentes.
+  { path: '**', redirectTo: 'products' },
 ];
