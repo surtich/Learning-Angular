6f903906f164b35ada8dbd286a0c29e8a5731d03
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-06 01:45:06 +0200

    ch12 - Se utilizan botones de alternancia para cambiar de la vista de tarjeta a la tabular.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,12 +8,18 @@ import { MatMiniFabButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import {
+  MatButtonToggle,
+  MatButtonToggleGroup,
+} from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-product-list',
   imports: [
     AsyncPipe,
     CurrencyPipe,
+    MatButtonToggle,
+    MatButtonToggleGroup,
     MatCardModule,
     MatIcon,
     MatMiniFabButton,
