c3bf536b86d2ae9301f4797409b393f10cbc6dc3
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-05 17:46:39 +0200

    ch11 -Se explica cómo manejar el error 401 No autorizado en Angular con el uso de un interceptor HTTP.

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,8 +1,32 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import {
+  HttpErrorResponse,
+  HttpInterceptorFn,
+  HttpStatusCode,
+} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { catchError, EMPTY, throwError } from 'rxjs';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const authReq = req.clone({
     setHeaders: { Authorization: 'myToken' },
   });
-  return next(authReq);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === HttpStatusCode.Unauthorized) {
+        authService.logout();
+        /*
+        El interceptor llamará al método `logout` de la clase `AuthService`
+        cuando ocurra un error 401 No autorizado y devolverá un observable
+        `EMPTY` para dejar de emitir datos. Utilizará el operador `throwError`
+        para propagar el error al manejador de errores global en todos los
+        demás errores. Como ya hemos visto, el manejador de errores global
+        examinará el error devuelto y actuará según el código de estado.
+        */
+        return EMPTY;
+      } else {
+        return throwError(() => error);
+      }
+    }),
+  );
 };
