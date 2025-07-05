74c68c70d2846571fcfa5aa577a81d2796e536f8
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-05 17:53:11 +0200

    ch11 - Se añade explicación sobre errores del framework Angular
    Se detalla el formato de los errores generados por Angular y su significado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS } from './app.settings';
@@ -16,6 +16,12 @@ import { AuthComponent } from './auth/auth.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  title = ''
   settings = inject(APP_SETTINGS);
+
+  ngAfterViewInit(): void {
+    // en la consola de desarrollador se muestra un error
+    this.title = this.settings.title;
+  }
 }
