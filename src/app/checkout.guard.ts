215403641e3f3b83788ca801d11a8e67d965f4f9
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-06 02:00:58 +0200

    ch12 - Se utiliza `MatDialog` para mostrar un cuadro de diálogo al usuario cuando hay productos en el carrito.

--- src/app/checkout.guard.ts
+++ src/app/checkout.guard.ts
@@ -2,14 +2,16 @@ import { CanDeactivateFn } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { inject } from '@angular/core';
 import { CartService } from './cart.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 export const checkoutGuard: CanDeactivateFn<CartComponent> = () => {
   const cartService = inject(CartService);
-  /* Ahora el `guard` solo muestra el mensaje de confirmación cuando el carrito tiene productos. */
+  const dialog = inject(MatDialog);
   if (cartService.cart) {
-    const confirmation = confirm(
-      'You have pending items in your cart. Do you want to continue?',
-    );
+    const confirmation = dialog
+      .open(CheckoutComponent, { data: cartService.cart.products.length })
+      .afterClosed();
     return confirmation;
   }
   return true;
