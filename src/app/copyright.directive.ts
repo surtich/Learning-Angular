182ae62884be011246f49592223c0ca3ff87166d
Javier Pérez Arteaga <surtich@gmail.com>
2025-07-06 19:01:08 +0200

    ch15 - Se soluciona el problema de hidratación del `copyright` detectando si la ejecución es en el navegador. Ya que el problema surgía cuando se intentaba acceder al DOM en el servidor, lo que no es posible. Ahora, el código solo se ejecuta en el navegador.

--- src/app/copyright.directive.ts
+++ src/app/copyright.directive.ts
@@ -1,13 +1,26 @@
-import { Directive, ElementRef } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import {
+  Directive,
+  ElementRef,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
 
 @Directive({
   selector: '[appCopyright]',
 })
-export class CopyrightDirective {
-  constructor(el: ElementRef) {
-    const currentYear = new Date().getFullYear();
-    const targetEl: HTMLElement = el.nativeElement;
-    targetEl.classList.add('copyright');
-    targetEl.textContent = `Copyright ©${currentYear} All Rights Reserved`;
+export class CopyrightDirective implements OnInit {
+  private platform = inject(PLATFORM_ID);
+  private el = inject(ElementRef);
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platform)) {
+      const currentYear = new Date().getFullYear();
+      const targetEl: HTMLElement = this.el.nativeElement;
+      targetEl.classList.add('copyright');
+      targetEl.textContent = `Copyright ©${currentYear} All Rights
+Reserved ${targetEl.textContent}`;
+    }
   }
 }
